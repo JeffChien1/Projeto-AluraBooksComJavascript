cfcfe3b711988bba19a1989caa0bc6c74c5848fe
Jeff Chien <eng.jeffchien@gmail.com>
2023-02-27 11:07:42 -0300

    Manipulando HTML com JavaScript

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,8 @@
 const elementoParaInserirLivros = document.getElementById('livros');
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirLivrosNaTela (listaDeLivros){
+    elementoComValorTotalDeLivrosDisponiveis = '';
     elementoParaInserirLivros.innerHTML = '';
     listaDeLivros.forEach(livro => {
 
