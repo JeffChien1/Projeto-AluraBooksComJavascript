d1a8c9f641438a3dcecd6bbb54f812fc806c9f7a
Jeff Chien <eng.jeffchien@gmail.com>
2023-02-28 11:47:58 -0300

    Arrumando e adicionando novas funções Reduce para somatoria de valores de livros disponiveis

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -7,9 +7,9 @@ function filtrarLivros() {
     let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
     exibirLivrosNaTela(livrosFiltrados);
     if(categoria == 'disponivel') {
-        exibirValorTotalDosLivrosDisponiveisNaTela();
+        const valorTotal = calculaValorTotalDeLivrosDisponiveis(livrosFiltrados);
+        exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
     }
-
 }
 
 function filtrarPorCategoria(categoria) {
@@ -20,11 +20,10 @@ function filtrarPorDisponibilidade() {
     return livros.filter(livro => livro.quantidade > 0);
 }
 
-function elementoComValorTotalDeLivrosDisponiveis (){
+function exibirValorTotalDosLivrosDisponiveisNaTela (valorTotal){
     elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
     <div class="livros__disponiveis">
-    <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
     </div>
-
     `
 }
